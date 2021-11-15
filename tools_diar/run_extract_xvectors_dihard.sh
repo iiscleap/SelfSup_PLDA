53457ae8e428bab109d31eaf9632c8b1018fe4c7
Prachi Singh <in-prsingh@in-prsinghs-MacBook-Pro.local>
2021-11-15 23:00:34 +0530

    codes updated

--- tools_diar/run_extract_xvectors_dihard.sh
+++ tools_diar/run_extract_xvectors_dihard.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-# This script is modified version of egs/callhome_diarization/v2/run.sh,
+# This script is modified version of https://github.com/dihardchallenge/dihard3_baseline/blob/master/recipes/track1/run.sh,
+# which is part of DIHARD III baseline.
 # Till stage 2, involves feature extraction and x-vector extraction using pretrained model
 # Stage 2 and Stage 3 involves converting x-vectors into numpy format and creating data folders needed for SSC training
 
@@ -10,7 +11,12 @@
 set -e
 mfccdir=`pwd`/mfcc
 vaddir=`pwd`/mfcc
-data_root=/export/corpora/NIST/LDC2001S97/   # callhome dataset path
+################################################################################
+# Paths to DIHARD III releases
+################################################################################
+DIHARD_DEV_DIR=/data/working/nryant/dihard3/delivery/builds/LDC2020E12_Third_DIHARD_Challenge_Development_Data
+DIHARD_EVAL_DIR=/data/working/nryant/dihard3/delivery/builds/LDC2021E02_Third_DIHARD_Challenge_Evaluation_Data_Complete
+
 stage=0
 nnet_dir=etdnn_fbank_xvector_models/exp/xvector_nnet_1a/  # path of xvector model
 conf=etdnn_fbank_xvector_models/conf
@@ -25,6 +31,28 @@ if [ $stage -le 0 ]; then
   if [ ! -d "steps" ];then
       ln -sf $KALDI_ROOT/egs/wsj/s5/steps .
   fi
+  
+  # dev
+  local/make_data_dir.py \
+  --rttm-dir $DIHARD_DEV_DIR/data/rttm \
+    data/dihard_dev_2020 \
+    $DIHARD_DEV_DIR/data/flac \
+    $DIHARD_DEV_DIR/data/sad
+  utils/utt2spk_to_spk2utt.pl \
+    data/dihard_dev_2020/utt2spk > data/dihard_dev_2020/spk2utt
+  ./utils/validate_data_dir.sh \
+    --no-text --no-feats data/dihard_dev_2020/
+
+  # eval
+  local/make_data_dir.py \
+    --rttm-dir $DIHARD_EVAL_DIR/data/rttm \
+    data/dihard_eval_2020 \
+    $DIHARD_EVAL_DIR/data/flac \
+    $DIHARD_EVAL_DIR/data/sad
+  utils/utt2spk_to_spk2utt.pl \
+    data/dihard_eval_2020/utt2spk > data/dihard_eval_2020/spk2utt
+  ./utils/validate_data_dir.sh \
+    --no-text --no-feats data/dihard_eval_2020/
 
 fi
 
